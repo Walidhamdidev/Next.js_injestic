eceb2921116fff23eccdceb3cedf0c29742f90fa
walid hamdi <waliddkz1@gmail.com>
2021-12-28 11:10:02 -0800

    change title

--- components/Layout.js
+++ components/Layout.js
@@ -1,17 +1,15 @@
-import React from 'react'
-import Footer from './Footer'
-import Nav from './Nav'
+import React from "react";
+import Footer from "./Footer";
+import Nav from "./Nav";
 
 export default function Layout({ children }) {
   return (
     <div className="flex flex-col justify-between min-h-screen">
       <Nav />
 
-      <main>
-        {children}
-      </main>
-      
+      <main>{children}</main>
+
       <Footer />
     </div>
-  )
+  );
 }
