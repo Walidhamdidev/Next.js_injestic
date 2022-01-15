2269df7dfcaf7648d26ce1b29b3a53f4d09a402a
walid-hamdi <walidhamdidev@gmail.com>
2022-01-14 20:40:13 -0800

    add navbar and banner

--- components/Layout.js
+++ components/Layout.js
@@ -1,14 +1,17 @@
 import React from "react";
 import Footer from "./Footer";
 import Nav from "./Nav";
+import NotificationBanner from "./NotificationBanner";
 
 export default function Layout({ children }) {
   return (
-    <div className="flex flex-col justify-between min-h-screen">
+    <div className="flex bg-[#f0f0f0] flex-col justify-between min-h-screen">
       <Nav />
 
-      <main>{children}</main>
-
+      <main>
+        <NotificationBanner />
+        {children}
+      </main>
       <Footer />
     </div>
   );
--- components/Nav.js
+++ components/Nav.js
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image";
 import { useContext } from "react";
 import { CartContext } from "../context/shopContext";
 import MiniCart from "./MiniCart";
@@ -12,19 +13,79 @@ export default function Nav() {
   });
 
   return (
-    <header className="border-b sticky top-0 z-20 bg-white">
-      <div className="flex items-center justify-between max-w-6xl pt-4 pb-2 px-4 mx-auto lg:max-w-screen-xl">
+    <header className="border-b  z-20 bg-[#f0f0f0]">
+      <div
+        className={`
+      flex items-center 
+      justify-between 
+      max-w-6xl py-5 px-10 
+      mx-auto 
+      lg:max-w-screen-xl`}
+      >
+        {" "}
         <Link href="/" passHref>
           <a className="cursor-pointer">
-            <span className="text-lg pt-1 font-bold">Injestic</span>
+            <span className="text-2xl pt-1 font-bold text-[#1e1a1a] uppercase">
+              Injestic
+            </span>
           </a>
         </Link>
-        <a
-          className="text-md font-bold cursor-pointer"
-          onClick={() => setCartOpen(!cartOpen)}
-        >
-          Cart ({cartQuantity})
-        </a>
+        <ul className="flex font-sans text-xl">
+          <li className="mr-10">
+            <Link href="#">
+              <a
+                href="#"
+                className="text-[#1e1a1a] pb-1 border-transparent hover:text-gray-800  hover:border-b-[1px] hover:border-[#1e1a1a] "
+              >
+                Home
+              </a>
+            </Link>
+          </li>
+          <li className="mr-10">
+            <Link href="#">
+              <a
+                href="#"
+                className="text-[#1e1a1a] pb-1 border-transparent hover:text-gray-800  hover:border-b-[1px] hover:border-[#1e1a1a]"
+              >
+                Store
+              </a>
+            </Link>
+          </li>
+          <li className="mr-10">
+            <Link href="#">
+              <a
+                href="#"
+                className="text-[#1e1a1a] pb-1 border-transparent hover:text-gray-800  hover:border-b-[1px] hover:border-[#1e1a1a]"
+              >
+                About
+              </a>
+            </Link>
+          </li>
+          <li className="mr-10">
+            <Link href="#">
+              <a
+                href="#"
+                className="text-[#1e1a1a] pb-1 border-transparent hover:text-gray-800  hover:border-b-[1px] hover:border-[#1e1a1a]"
+              >
+                Account
+              </a>
+            </Link>
+          </li>
+          <Link href={""}>
+            <a
+              className="text-md  cursor-pointer relative w-8 h-8"
+              onClick={() => setCartOpen(!cartOpen)}
+              href={""}
+            >
+              <Image layout="fill" src="/images/shopping_cart.svg" />
+              {cartQuantity > 0 && (
+                <div className="absolute -top-3 -right-3 bg-[#1e1a1a] w-5 h-5 text-white rounded-full flex justify-center items-center">
+                  <p className="font-bold text-sm">{cartQuantity}</p>
+                </div>
+              )}
+            </a>
+          </Link>
+        </ul>
         <MiniCart cart={cart} />
       </div>
     </header>
