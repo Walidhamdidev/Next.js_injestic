08dbdc3e5f820d33eb2d69e9a844158f60efaf87
walid hamdi <walidhamdidev@gmail.com>
2023-04-06 17:47:38 +0100

    handle tracking by id page

--- components/OrderTrackingCom.jsx
+++ components/OrderTrackingCom.jsx
@@ -1,27 +1,28 @@
 import { useState } from "react";
+import { getProduct } from "../lib/shopify";
 
 export function OrderTrackingCom() {
-  const [track, setTrack] = useState("");
+  const [orderId, setOrderId] = useState("");
+  const [email, setEmail] = useState("");
+  const [orderDetails, setOrderDetails] = useState(null);
+  const [errorMessage, setErrorMessage] = useState("");
 
-  const handleChange = ({ currentTarget }) => {
-    const value = currentTarget.value;
-
-    setTrack(value);
-  };
-
-  const doTrack = () => {
-    if (track === "") {
-      alert("Enter your number.");
+  const handleTrackClick = async () => {
+    if (!orderId || !email) {
+      setErrorMessage("Please enter your order ID.");
       return;
     }
 
-    YQV5.trackSingle({
-      YQ_ContainerId: "YQContainer",
-      YQ_Height: 560,
-      YQ_Fc: "0",
-      YQ_Lang: "en",
-      YQ_Num: track,
-    });
+    const product = await getProduct(orderId);
+
+    if (!product) {
+      setErrorMessage("No order found with the specified ID.");
+      setOrderDetails(null);
+      return;
+    }
+
+    setOrderDetails(product);
+    setErrorMessage("");
   };
 
   return (
@@ -38,20 +39,38 @@ export function OrderTrackingCom() {
         className="bg-gray-50 outline-none  border-yellow-200 rounded-full ml-1 mt-10 block px-10 py-2"
         type="text"
         autoFocus
-        value={track}
-        onChange={(e) => handleChange(e)}
-        placeholder="search..."
+        value={orderId}
+        onChange={(e) => setOrderId(e.target.value)}
+        placeholder="Search by track ID"
       />
 
       <div className="inline-block align-bottom mt-10 text-sm">
         <button
-          onClick={(e) => doTrack(e)}
+          onClick={handleTrackClick}
           className="bg-yellow-300 opacity-75 hover:opacity-100 text-yellow-900 hover:text-gray-900 rounded-full px-10 py-2 font-semibold"
         >
           <i className="fa fa-shopping-cart -ml-2 mr-2" aria-hidden="true"></i>
           Track
         </button>
       </div>
+
+      {errorMessage && <p className="mt-5">{errorMessage}</p>}
+      {orderDetails && (
+        <div>
+          <h2>Order Details</h2>
+          <p>Order ID: {orderDetails.id}</p>
+          <p>Product Name: {orderDetails.title}</p>
+          <p>
+            Price: {orderDetails.variants[0].priceV2.amount}{" "}
+            {orderDetails.variants[0].priceV2.currencyCode}
+          </p>
+          <p>Image:</p>
+          <img
+            src={orderDetails.images[0].originalSrc}
+            alt={orderDetails.images[0].altText}
+          />
+        </div>
+      )}
     </div>
   );
 }
